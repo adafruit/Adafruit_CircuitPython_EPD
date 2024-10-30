29cfedcfe467671d53287869c176cb8cae938399
Mikey Sklar <mikeysklar@gmail.com>
2024-10-30 10:21:43 -0700

    back to original
    plus 1680Z subclass at bottom

--- adafruit_epd/ssd1680.py
+++ adafruit_epd/ssd1680.py
@@ -6,52 +6,97 @@
 `adafruit_epd.ssd1680` - Adafruit SSD1680 - ePaper display driver
 ====================================================================================
 CircuitPython driver for Adafruit SSD1680 display breakouts
-* Author(s): Melissa LeBlanc-Williams Mikey Sklar
+* Author(s): Melissa LeBlanc-Williams
 """
+
 import time
 from micropython import const
 import adafruit_framebuf
 from adafruit_epd.epd import Adafruit_EPD
 
-# Define all SSD1680 constants
+try:
+    """Needed for type annotations"""
+    import typing  # pylint: disable=unused-import
+    from typing_extensions import Literal
+    from busio import SPI
+    from digitalio import DigitalInOut
+
+except ImportError:
+    pass
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_EPD.git"
+
 _SSD1680_DRIVER_CONTROL = const(0x01)
+_SSD1680_GATE_VOLTAGE = const(0x03)
+_SSD1680_SOURCE_VOLTAGE = const(0x04)
+_SSD1680_INIT_SETTING = const(0x08)
+_SSD1680_INIT_WRITE_REG = const(0x09)
+_SSD1680_INIT_READ_REG = const(0x0A)
+_SSD1680_BOOSTER_SOFT_START = const(0x0C)
+_SSD1680_DEEP_SLEEP = const(0x10)
 _SSD1680_DATA_MODE = const(0x11)
 _SSD1680_SW_RESET = const(0x12)
-_SSD1680_SET_RAMXPOS = const(0x44)
-_SSD1680_SET_RAMYPOS = const(0x45)
+_SSD1680_HV_DETECT = const(0x14)
+_SSD1680_VCI_DETECT = const(0x15)
+_SSD1680_TEMP_CONTROL = const(0x18)
+_SSD1680_TEMP_WRITE = const(0x1A)
+_SSD1680_TEMP_READ = const(0x1B)
+_SSD1680_EXTTEMP_WRITE = const(0x1C)
+_SSD1680_MASTER_ACTIVATE = const(0x20)
+_SSD1680_DISP_CTRL1 = const(0x21)
+_SSD1680_DISP_CTRL2 = const(0x22)
 _SSD1680_WRITE_BWRAM = const(0x24)
 _SSD1680_WRITE_REDRAM = const(0x26)
+_SSD1680_READ_RAM = const(0x27)
+_SSD1680_VCOM_SENSE = const(0x28)
+_SSD1680_VCOM_DURATION = const(0x29)
+_SSD1680_WRITE_VCOM_OTP = const(0x2A)
+_SSD1680_WRITE_VCOM_CTRL = const(0x2B)
+_SSD1680_WRITE_VCOM_REG = const(0x2C)
+_SSD1680_READ_OTP = const(0x2D)
+_SSD1680_READ_USERID = const(0x2E)
+_SSD1680_READ_STATUS = const(0x2F)
+_SSD1680_WRITE_WS_OTP = const(0x30)
+_SSD1680_LOAD_WS_OTP = const(0x31)
+_SSD1680_WRITE_LUT = const(0x32)
+_SSD1680_CRC_CALC = const(0x34)
+_SSD1680_CRC_READ = const(0x35)
+_SSD1680_PROG_OTP = const(0x36)
+_SSD1680_WRITE_DISPLAY_OPT = const(0x37)
+_SSD1680_WRITE_USERID = const(0x38)
+_SSD1680_OTP_PROGMODE = const(0x39)
+_SSD1680_WRITE_BORDER = const(0x3C)
+_SSD1680_END_OPTION = const(0x3F)
+_SSD1680_SET_RAMXPOS = const(0x44)
+_SSD1680_SET_RAMYPOS = const(0x45)
+_SSD1680_AUTOWRITE_RED = const(0x46)
+_SSD1680_AUTOWRITE_BW = const(0x47)
 _SSD1680_SET_RAMXCOUNT = const(0x4E)
 _SSD1680_SET_RAMYCOUNT = const(0x4F)
-_SSD1680_DISP_CTRL2 = const(0x22)
-_SSD1680_MASTER_ACTIVATE = const(0x20)
-_SSD1680_DEEP_SLEEP = const(0x10)
+_SSD1680_NOP = const(0xFF)
 
 
 class Adafruit_SSD1680(Adafruit_EPD):
-    """Driver for SSD1680 ePaper display, default driver."""
+    """driver class for Adafruit SSD1680 ePaper display breakouts"""
 
-    # pylint: disable=too-many-arguments, useless-parent-delegation
+    # pylint: disable=too-many-arguments
     def __init__(
-        self, width, height, spi, *, cs_pin, dc_pin, sramcs_pin, rst_pin, busy_pin
-    ):
-        # Call parent class's __init__ to initialize sram and other attributes
+        self,
+        width: int,
+        height: int,
+        spi: SPI,
+        *,
+        cs_pin: DigitalInOut,
+        dc_pin: DigitalInOut,
+        sramcs_pin: DigitalInOut,
+        rst_pin: DigitalInOut,
+        busy_pin: DigitalInOut
+    ) -> None:
         super().__init__(
             width, height, spi, cs_pin, dc_pin, sramcs_pin, rst_pin, busy_pin
         )
 
-        self.cs_pin = cs_pin
-        self.dc_pin = dc_pin
-        self.sramcs_pin = sramcs_pin
-        self.rst_pin = rst_pin
-        self.busy_pin = busy_pin
-
-        self.initialize_buffers(width, height)
-
-    # pylint: enable=too-many-arguments, useless-parent-delegation
-
-    def initialize_buffers(self, width, height):
-        """Initialize width height stride buffers"""
         stride = width
         if stride % 8 != 0:
             stride += 8 - stride % 8
@@ -59,7 +104,7 @@ class Adafruit_SSD1680(Adafruit_EPD):
         self._buffer1_size = int(stride * height / 8)
         self._buffer2_size = self._buffer1_size
 
-        if self.sramcs_pin:
+        if sramcs_pin:
             self._buffer1 = self.sram.get_view(0)
             self._buffer2 = self.sram.get_view(self._buffer1_size)
         else:
@@ -67,67 +112,105 @@ class Adafruit_SSD1680(Adafruit_EPD):
             self._buffer2 = bytearray(self._buffer2_size)
 
         self._framebuf1 = adafruit_framebuf.FrameBuffer(
-            self._buffer1, width, height, buf_format=adafruit_framebuf.MHMSB
+            self._buffer1,
+            width,
+            height,
+            stride=stride,
+            buf_format=adafruit_framebuf.MHMSB,
         )
         self._framebuf2 = adafruit_framebuf.FrameBuffer(
-            self._buffer2, width, height, buf_format=adafruit_framebuf.MHMSB
+            self._buffer2,
+            width,
+            height,
+            stride=stride,
+            buf_format=adafruit_framebuf.MHMSB,
         )
         self.set_black_buffer(0, True)
         self.set_color_buffer(1, False)
+        # pylint: enable=too-many-arguments
 
-    def busy_wait(self):
-        """Wait for the display to complete its current task."""
-        if self.busy_pin:
-            while self.busy_pin.value:
+    def begin(self, reset: bool = True) -> None:
+        """Begin communication with the display and set basic settings"""
+        if reset:
+            self.hardware_reset()
+        self.power_down()
+
+    def busy_wait(self) -> None:
+        """Wait for display to be done with current task, either by polling the
+        busy pin, or pausing"""
+        if self._busy:
+            while self._busy.value:
                 time.sleep(0.01)
         else:
             time.sleep(0.5)
 
-    def power_up(self):
-        """Power up sequence for SSD1680."""
+    def power_up(self) -> None:
+        """Power up the display in preparation for writing RAM and updating"""
         self.hardware_reset()
         self.busy_wait()
         self.command(_SSD1680_SW_RESET)
         self.busy_wait()
-
+        # driver output control
         self.command(
             _SSD1680_DRIVER_CONTROL,
             bytearray([self._height - 1, (self._height - 1) >> 8, 0x00]),
         )
+        # data entry mode
         self.command(_SSD1680_DATA_MODE, bytearray([0x03]))
+
+        # Set voltages
+        self.command(_SSD1680_WRITE_VCOM_REG, bytearray([0x36]))
+        self.command(_SSD1680_GATE_VOLTAGE, bytearray([0x17]))
+        self.command(_SSD1680_SOURCE_VOLTAGE, bytearray([0x41, 0x00, 0x32]))
+
+        # Set ram X start/end postion
         self.command(_SSD1680_SET_RAMXPOS, bytearray([0x01, 0x10]))
+        # Set ram Y start/end postion
         self.command(
             _SSD1680_SET_RAMYPOS,
             bytearray([0, 0, self._height - 1, (self._height - 1) >> 8]),
         )
+        # Set border waveform
+        self.command(_SSD1680_WRITE_BORDER, bytearray([0x05]))
 
-    def write_ram(self, index):
-        """Write to RAM for SSD1680."""
+        # Set ram X count
+        self.command(_SSD1680_SET_RAMXCOUNT, bytearray([0x01]))
+        # Set ram Y count
+        self.command(_SSD1680_SET_RAMYCOUNT, bytearray([self._height - 1, 0]))
+        self.busy_wait()
+
+    def power_down(self) -> None:
+        """Power down the display - required when not actively displaying!"""
+        self.command(_SSD1680_DEEP_SLEEP, bytearray([0x01]))
+        time.sleep(0.1)
+
+    def update(self) -> None:
+        """Update the display from internal memory"""
+        self.command(_SSD1680_DISP_CTRL2, bytearray([0xF4]))
+        self.command(_SSD1680_MASTER_ACTIVATE)
+        self.busy_wait()
+        if not self._busy:
+            time.sleep(3)  # wait 3 seconds
+
+    def write_ram(self, index: Literal[0, 1]) -> int:
+        """Send the one byte command for starting the RAM write process. Returns
+        the byte read at the same time over SPI. index is the RAM buffer, can be
+        0 or 1 for tri-color displays."""
         if index == 0:
             return self.command(_SSD1680_WRITE_BWRAM, end=False)
         if index == 1:
             return self.command(_SSD1680_WRITE_REDRAM, end=False)
-
-        # Raise an error if the index is not 0 or 1
         raise RuntimeError("RAM index must be 0 or 1")
 
-    def set_ram_address(self, x, y):
-        """Set RAM address location for SSD1680."""
+    def set_ram_address(
+        self, x: int, y: int
+    ) -> None:  # pylint: disable=unused-argument, no-self-use
+        """Set the RAM address location, not used on this chipset but required by
+        the superclass"""
+        # Set RAM X address counter
         self.command(_SSD1680_SET_RAMXCOUNT, bytearray([x + 1]))
+        # Set RAM Y address counter
         self.command(_SSD1680_SET_RAMYCOUNT, bytearray([y, y >> 8]))
-
-    def update(self):
-        """Update the display from internal memory."""
-        self.command(_SSD1680_DISP_CTRL2, bytearray([0xF4]))  # Full update
-        self.command(_SSD1680_MASTER_ACTIVATE)
-        self.busy_wait()
-        if not self.busy_pin:
-            time.sleep(3)  # Wait for update to complete
-
-    def power_down(self):
-        """Power down the display."""
-        self.command(_SSD1680_DEEP_SLEEP, bytearray([0x01]))
-        time.sleep(0.1)
 
 
 class Adafruit_SSD1680Z(Adafruit_SSD1680):
@@ -148,6 +231,7 @@ class Adafruit_SSD1680Z(Adafruit_SSD1680):
             rst_pin=rst_pin,
             busy_pin=busy_pin,
         )
+        self.busy_pin = busy_pin  # Ensure busy_pin is set
 
     # pylint: enable=too-many-arguments, useless-parent-delegation
 
