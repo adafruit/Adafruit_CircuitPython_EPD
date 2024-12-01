1be3100af828e65f889f692bb59eca509bcb16ce
Mikey Sklar <mikeysklar@gmail.com>
2024-12-01 12:56:07 -0800

    Update adafruit_epd/ssd1680.py
    Co-authored-by: Dan Halbert <halbert@halwitz.org>

--- adafruit_epd/ssd1680.py
+++ adafruit_epd/ssd1680.py
@@ -24,7 +24,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "2.13.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_EPD.git"
 
 _SSD1680_DRIVER_CONTROL = const(0x01)
